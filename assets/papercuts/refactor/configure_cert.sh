77c4835ea05391656dbc8df15f29d3af04de0d31
Samuel-L <Samuel.L@ibm.com>
2024-03-12 21:39:45 -0400

    Update configure_cert.sh
    Update .gitignore
    
    Update configure_cert.sh
    
    Update configure_cert.sh

--- assets/papercuts/refactor/configure_cert.sh
+++ assets/papercuts/refactor/configure_cert.sh
@@ -1,21 +1,17 @@
 #Load .env
 set -o allexport
 source .env set
-+o allexport
 
+#copy config template and edit
+cp refactoring_assistant/config/template.properties refactoring_assistant/config/config.properties
 
-echo $CERTS_LOCATION
-echo $HOST_PORT
-echo $ENVIRONMENT_ID
-
-#copy config templat
-e and edit
-cp -a 'refactoring_assistant/config/template.properties' 'refactoring_assistant/config/config.properties'
+chmod 766 refactoring_assistant/config/config.properties
 
 sed -i "s@CERTS_LOCATION=.*@CERTS_LOCATION=$CERTS_LOCATION@" config.properties
 sed -i "s@HOST_PORT=.*@HOST_PORT=$HOST_PORT@" config.properties
 sed -i "s@ENVIRONMENT_ID=.*@ENVIRONMENT_ID==$ENVIRONMENT_ID@" config.properties
 
-chmod 766 /refactoring_assistant/scripts/nls.sh
+chmod 766 refactoring_assistant/scripts/nls.sh
 
-#./refactoring_assistant/start.sh --prepare-only /root/certs
+cd refactoring_assistant
+./start.sh --prepare-only /root/certs
