de95fe9c4456d01f23dac7b51f8e5b4a4bd3edcb
oscar-ricaud <oscar.ricaud@ibm.com>
2024-03-13 13:10:49 -0500

    update on path to use absoloute vs relative on tls_certs_configuration.ps1

--- assets/papercuts/addi/tls/tls_certs_configuration.ps1
+++ assets/papercuts/addi/tls/tls_certs_configuration.ps1
@@ -1,7 +1,7 @@
 # import the common functions script
-. ".\shared\common_functions.ps1"
-. ".\shared\common_functions_tls.ps1"
-. ".\shared\get_set_env_vars.ps1"
+. "assets/papercuts/addi/SQL_Server/shared/common_functions.ps1"
+. "assets/papercuts/addi/SQL_Server/shared/common_functions.ps1"
+. "assets/papercuts/addi/SQL_Server/shared/get_set_env_vars.ps1"
 
 function Main {
     Writing-Host "Configure TLS Certs and Keystores"
