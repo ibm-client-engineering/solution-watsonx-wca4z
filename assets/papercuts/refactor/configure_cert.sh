12841306bf8e792ea7bf3ea7a19925db035dcdb2
Samuel-L <Samuel.L@ibm.com>
2024-03-12 21:26:48 -0400

    fixed some location issues

--- assets/papercuts/refactor/configure_cert.sh
+++ assets/papercuts/refactor/configure_cert.sh
@@ -4,9 +4,10 @@ source .env set
 +o allexport
 
 #copy config template and edit
-cp 'IBM watsonx Code Assistant for Z Refactoring Assistant 1.1.0 Linux Multilingual/config/template.properties' 'IBM watsonx Code Assistant for Z Refactoring Assistant 1.1.0 Linux Multilingual/config/config.properties'
+cp 'refactoring_assistant/config/template.properties' 'refactoring_assistant/config/config.properties'
 
 sed -i "s@CERTS_LOCATION=.*@CERTS_LOCATION=$CERTS_LOCATION@" config.properties
 sed -i "s@HOST_PORT=.*@HOST_PORT=$HOST_PORT@" config.properties
 sed -i "s@ENVIRONMENT_ID=.*@ENVIRONMENT_ID==$ENVIRONMENT_ID@" config.properties
 
+./refactoring_assistant/start.sh --prepare-only /root/certs
