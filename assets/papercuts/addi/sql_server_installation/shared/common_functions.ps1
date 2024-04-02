43439a99e842adadb10de57703cd417a0c18977d
oscar-ricaud <oscar.ricaud@ibm.com>
2024-04-02 16:27:51 -0600

    update on common_functions.ps1

--- assets/papercuts/addi/sql_server_installation/shared/common_functions.ps1
+++ assets/papercuts/addi/sql_server_installation/shared/common_functions.ps1
@@ -148,6 +148,9 @@ function ConfirmAndExecute {
 function InstallODBCDriver {
     $url = "https://go.microsoft.com/fwlink/?linkid=2249004"
     $outputPath = "./msodbcsql.msi"
-    Invoke-WebRequest -Uri $url -OutFile $outputPath
-    Start-Process -FilePath $outputPath -Wait
+
+    if (-not (Test-Path $outputPath)) {
+        Invoke-WebRequest -Uri $url -OutFile $outputPath
+        Start-Process -FilePath $outputPath -Wait
+    }
 }
