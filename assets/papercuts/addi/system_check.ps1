12e9e26dc229790dfe19e2fe2f8bfa9a9fe6990c
Samuel-L <Samuel.L@ibm.com>
2024-04-02 15:57:03 -0400

    updated preflight check to warn instead of throwing errors

--- assets/papercuts/addi/system_check.ps1
+++ assets/papercuts/addi/system_check.ps1
@@ -6,17 +6,17 @@ $OS = Get-WmiObject -Class Win32_OperatingSystem
 $Disk = Get-WmiObject -Class Win32_LogicalDisk -Filter "DeviceID='C:'"
 
 if ($CPU.Name -notmatch 'Intel\(R\) Core\(TM\) i5|Intel\(R\) Core\(TM\) i7|Intel\(R\) Core\(TM\) i9|Intel Xeon') {
-    throw 'CPU is not Intel Core i5 or equivalent'
+    Write-Warning "CPU is not Intel Core i5 or equivalent (CPU is $CPU.Name)"
 }
 
 if ($RAM.Sum -lt 32GB) {
-    throw 'RAM is less than 32GB'
+    Write-Warning 'RAM is less than 32GB'
 }
 
 if ($OS.Caption -notmatch 'Windows 10|Windows 11|Windows Server 2012|Windows Server 2016|Windows Server 2019|Windows Server 2022') {
-    throw 'OS is not Windows 10/11 (64-bit) or Windows Server 2012/2016/2019/2022'
+    Write-Warning 'OS is not Windows 10/11 (64-bit) or Windows Server 2012/2016/2019/2022'
 }
 
 if ($Disk.FreeSpace -lt 35GB) {
-    throw 'Disk space is less than 35GB'
+    Write-Warning 'Disk space is less than 35GB'
 }
