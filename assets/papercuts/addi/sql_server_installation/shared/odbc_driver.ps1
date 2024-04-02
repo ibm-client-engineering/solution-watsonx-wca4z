3131d9523d5f719ec50974c381f24118ff1efc49
oscar-ricaud <oscar.ricaud@ibm.com>
2024-04-02 14:49:20 -0600

    adding the odbc driver

--- assets/papercuts/addi/sql_server_installation/shared/odbc_driver.ps1
+++ assets/papercuts/addi/sql_server_installation/shared/odbc_driver.ps1
@@ -1,3 +1,4 @@
 $url = "https://go.microsoft.com/fwlink/?linkid=2249004"
 $outputPath = "./msodbcsql.msi"
 Invoke-WebRequest -Uri $url -OutFile $outputPath
+Start-Process -FilePath $outputPath -Wait
