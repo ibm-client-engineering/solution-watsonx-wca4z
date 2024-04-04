1d5f5198020235f0983be21cc30503cb60810f7e
oscar-ricaud <oscar.ricaud@ibm.com>
2024-04-04 13:51:19 -0600

    revert

--- assets/papercuts/addi/addi_installation/install_addi.ps1
+++ assets/papercuts/addi/addi_installation/install_addi.ps1
@@ -4,7 +4,6 @@ function DownloadBinary {
     )
 
     # Download binary
-    $ProgressPreference = 'SilentlyContinue'
     Invoke-WebRequest -Uri $url -OutFile "addi_endpoint_install_binary.zip"
 
     # Unzip the binary
@@ -13,6 +12,7 @@ function DownloadBinary {
     # Cleanup remove the downloaded zip file
     Remove-Item "addi_endpoint_install_binary.zip" -Force
 }
+# Function is responsible given the values on .env it updates the CCS_IP and CCS_PORT on the auto-install.xml
 function UpdateXmlValues {
     $envFilePath = "./.env"
     $xmlFilePath = "./auto-install.xml"
@@ -20,47 +20,33 @@ function UpdateXmlValues {
     # Read the .env file and set the env vars
     Get-Content $envFilePath | ForEach-Object {
         $envVar = $_ -split '=', 2
-        $envVarName = $envVar[0].Trim()
-        $envVarValue = $envVar[1].Trim()
-        [System.Environment]::SetEnvironmentVariable($envVarName, $envVarValue, [System.EnvironmentVariableTarget]::Process)
+        [System.Environment]::SetEnvironmentVariable($envVar[0], $envVar[1], [System.EnvironmentVariableTarget]::Process)
     }
+    Write-Host "CCS_IP from .env: $($env:ccsIP)"
+    Write-Host "CCS_PORT from .env: $($env:ccsPort)"
 
-    Write-Host "CCS_IP from .env: $($env:CCS_IP)"
-    Write-Host "CCS_PORT from .env: $($env:CCS_PORT)"
+    echo "Starting process to install ADDI_FOR_IBM_Z_612_WIN.zip"
 
-    # Load XML file
-    $xml = New-Object XML
-    $xml.Load($xmlFilePath)
+    $xml = [xml](Get-Content -Path $xmlFilePath)
 
-    # Update CCS_IP and CCS_PORT values
-    $userInputPanel = $xml.SelectSingleNode("//com.izforge.izpack.panels.userinput.UserInputPanel[@id='userInput']")
+    # find the specific userinput panel with id="userInput" and update the CCS_IP key
+    $userInputPanel = $xml.AutomatedInstallation.'com.izforge.izpack.panels.userinput.UserInputPanel' | Where-Object { $_.id -eq "userInput"}
+
     if ($userInputPanel -ne $null) {
-        $ccsIPNode = $userInputPanel.SelectSingleNode("entry[@key='CCS_IP']")
-        if ($ccsIPNode -ne $null) {
-            $ccsIPNode.SetAttribute('value', $env:CCS_IP)
-        }
-
-        $ccsPortNode = $userInputPanel.SelectSingleNode("entry[@key='CCS_PORT']")
-        if ($ccsPortNode -ne $null) {
-            $ccsPortNode.SetAttribute('value', $env:CCS_PORT)
-        }
+        $userInputPanel.entry | Where-Object { $_.key -eq "CCS_IP" } | ForEach-Object { $_.value = $($env:ccsIP) }
     }
-
-    # Save XML file
+    if ($userInputPanel -ne $null) {
+        $userInputPanel.entry | Where-Object { $_.key -eq "CCS_PORT" } | ForEach-Object { $_.value = $($env:ccsPort) }
+    }
     $xml.Save($xmlFilePath)
 }
 
 function Main {
     UpdateXmlValues
-    $xmlFilePath = "./auto-install.xml"
-#     $addi_endpoint_install_binary="https://papercuts-wca4z.s3.us-south.cloud-object-storage.appdomain.cloud/ADDI_FOR_IBM_Z_612_WIN.zip"
-#     if (!(Test-Path "unzipped_binary")) {
-#         DownloadBinary -url $addi_endpoint_install_binary
-#     }
+    #$addi_endpoint_install_binary="https://papercuts-wca4z.s3.us-south.cloud-object-storage.appdomain.cloud/ADDI_FOR_IBM_Z_612_WIN.zip"
+    #DownloadBinary -url $addi_endpoint_install_binary
     ls
-    # java -jar '.\unzipped_binary\IBM ADDI\IBM_Application_Discovery_and_Delivery_Intelligence_Installer-6.1.2-ifix2.exe' -f $xmlFilePath
-
-    #start microsoft-edge:https://localhost:9443/ad/admin/setup?step=1
+    #java -jar .\IBM_Application_Discovery_and_Delivery_Intelligence_Installer-6.1.2-ifix1.exe -f $xmlFilePath
 }
 
 Main
