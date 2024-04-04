af6ef34968d4e6c89a3c1e7e498db542e314428e
oscar-ricaud <oscar.ricaud@ibm.com>
2024-04-04 13:41:33 -0600

    update on install_addi.ps1

--- assets/papercuts/addi/addi_installation/install_addi.ps1
+++ assets/papercuts/addi/addi_installation/install_addi.ps1
@@ -39,7 +39,7 @@ function UpdateXmlValues {
         $ccsIPNode = $userInputPanel.SelectSingleNode("//entry[@key='CCS_IP']")
         if ($ccsIPNode -ne $null) {
             Write-Host "Found CCS_IP node"
-            $ccsIPNode.SetAttribute('value', $env:CCS_IP)
+            $ccsIPNode.SetAttribute('value', "omg")
         }
         else {
             Write-Host "CCS_IP node not found"
@@ -48,7 +48,7 @@ function UpdateXmlValues {
         $ccsPortNode = $userInputPanel.SelectSingleNode("//entry[@key='CCS_PORT']")
         if ($ccsPortNode -ne $null) {
             Write-Host "Found CCS_PORT node"
-            $ccsPortNode.SetAttribute('value', $env:CCS_PORT)
+            $ccsPortNode.SetAttribute('value', "omg")
         }
         else {
             Write-Host "CCS_PORT node not found"
