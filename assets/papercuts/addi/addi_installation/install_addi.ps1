69c84c5efa1ce15c57ec6b85e9b8a1898b0d59c1
oscar-ricaud <oscar.ricaud@ibm.com>
2024-04-04 13:57:47 -0600

    revert

--- assets/papercuts/addi/addi_installation/install_addi.ps1
+++ assets/papercuts/addi/addi_installation/install_addi.ps1
@@ -13,33 +13,33 @@ function DownloadBinary {
     Remove-Item "addi_endpoint_install_binary.zip" -Force
 }
 # Function is responsible given the values on .env it updates the CCS_IP and CCS_PORT on the auto-install.xml
-function UpdateXmlValues {
-    $envFilePath = "./.env"
-    $xmlFilePath= "./auto-install.xml"
+ function UpdateXmlValues {
+     $envFilePath = "./.env"
+     $xmlFilePath="./auto-install.xml"
 
-    # Read the .env file and set the env vars
-    Get-Content $envFilePath | ForEach-Object {
-        $envVar = $_ -split '=', 2
-        [System.Environment]::SetEnvironmentVariable($envVar[0], $envVar[1], [System.EnvironmentVariableTarget]::Process)
-    }
-    Write-Host "CCS_IP from .env: $($env:ccsIP)"
-    Write-Host "CCS_PORT from .env: $($env:ccsPort)"
+     # Read the .env file and set the env vars
+     Get-Content $envFilePath | ForEach-Object {
+         $envVar = $_ -split '=', 2
+         [System.Environment]::SetEnvironmentVariable($envVar[0], $envVar[1], [System.EnvironmentVariableTarget]::Process)
+     }
+     Write-Host "CCS_IP from .env: $($env:ccsIP)"
+     Write-Host "CCS_PORT from .env: $($env:ccsPort)"
 
-    echo "Starting process to install ADDI_FOR_IBM_Z_612_WIN.zip"
+     echo "Starting process to install ADDI_FOR_IBM_Z_612_WIN.zip"
 
-    $xml = [xml](Get-Content -Path $xmlFilePath)
+     $xml = [xml](Get-Content -Path $xmlFilePath)
 
-    # find the specific userinput panel with id="userInput" and update the CCS_IP key
-    $userInputPanel = $xml.AutomatedInstallation.'com.izforge.izpack.panels.userinput.UserInputPanel' | Where-Object { $_.id -eq "userInput"}
+     # find the specific userinput panel with id="userInput" and update the CCS_IP key
+     $userInputPanel = $xml.AutomatedInstallation.'com.izforge.izpack.panels.userinput.UserInputPanel' | Where-Object { $_.id -eq "userInput"}
 
-    if ($userInputPanel -ne $null) {
-        $userInputPanel.entry | Where-Object { $_.key -eq "CCS_IP" } | ForEach-Object { $_.value = $($env:ccsIP) }
-    }
-    if ($userInputPanel -ne $null) {
-        $userInputPanel.entry | Where-Object { $_.key -eq "CCS_PORT" } | ForEach-Object { $_.value = $($env:ccsPort) }
-    }
-    $xml.Save($xmlFilePath)
-}
+     if ($userInputPanel -ne $null) {
+         $userInputPanel.entry | Where-Object { $_.key -eq "CCS_IP" } | ForEach-Object { $_.value = $($env:ccsIP) }
+     }
+     if ($userInputPanel -ne $null) {
+         $userInputPanel.entry | Where-Object { $_.key -eq "CCS_PORT" } | ForEach-Object { $_.value = $($env:ccsPort) }
+     }
+     $xml.Save($xmlFilePath)
+ }
 
 function Main {
     UpdateXmlValues
