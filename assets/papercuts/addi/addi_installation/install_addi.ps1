1659ff94fedb444825a9a1677cd30fa278362e4d
oscar-ricaud <oscar.ricaud@ibm.com>
2024-04-04 12:30:24 -0600

    update on install_addi.ps1

--- assets/papercuts/addi/addi_installation/install_addi.ps1
+++ assets/papercuts/addi/addi_installation/install_addi.ps1
@@ -4,7 +4,6 @@ function DownloadBinary {
     )
 
     # Download binary
-    $ProgressPreference = 'SilentlyContinue'
     Invoke-WebRequest -Uri $url -OutFile "addi_endpoint_install_binary.zip"
 
     # Unzip the binary
@@ -44,15 +43,10 @@ function UpdateXmlValues {
 
 function Main {
     UpdateXmlValues
-    $xmlFilePath = "./auto-install.xml"
-#     $addi_endpoint_install_binary="https://papercuts-wca4z.s3.us-south.cloud-object-storage.appdomain.cloud/ADDI_FOR_IBM_Z_612_WIN.zip"
-#     if (!(Test-Path "unzipped_binary")) {
-#         DownloadBinary -url $addi_endpoint_install_binary
-#     }
+    #$addi_endpoint_install_binary="https://papercuts-wca4z.s3.us-south.cloud-object-storage.appdomain.cloud/ADDI_FOR_IBM_Z_612_WIN.zip"
+    #DownloadBinary -url $addi_endpoint_install_binary
     ls
-    # java -jar '.\unzipped_binary\IBM ADDI\IBM_Application_Discovery_and_Delivery_Intelligence_Installer-6.1.2-ifix2.exe' -f $xmlFilePath
-
-    #start microsoft-edge:https://localhost:9443/ad/admin/setup?step=1
+    #java -jar .\IBM_Application_Discovery_and_Delivery_Intelligence_Installer-6.1.2-ifix1.exe -f $xmlFilePath
 }
 
 Main
