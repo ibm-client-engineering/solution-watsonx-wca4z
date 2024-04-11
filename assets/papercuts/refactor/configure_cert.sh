1ea662454619b4eab6e12862dcae100a6de34a90
oscar-ricaud <oscar.ricaud@ibm.com>
2024-04-11 14:47:40 -0500

    fixed issue on configure_cert.sh

--- assets/papercuts/refactor/configure_cert.sh
+++ assets/papercuts/refactor/configure_cert.sh
@@ -3,15 +3,15 @@ set -o allexport
 source .env set
 
 #copy config template and edit
-cp refactoring_assistant/config/template.properties refactoring_assistant/config/config.properties
+cp /opt/refactoring-assistant/refactoring-assistant/config/template.properties /opt/refactoring-assistant/refactoring-assistant/config/config.properties
 
 chmod 766 refactoring_assistant/config/config.properties
 
-sed -i "s@CERTS_LOCATION=.*@CERTS_LOCATION=$CERTS_LOCATION@" refactoring_assistant/config/config.properties
-sed -i "s@HOST_PORT=.*@HOST_PORT=$HOST_PORT@" refactoring_assistant/config/config.properties
-sed -i "s@ENVIRONMENT_ID=.*@ENVIRONMENT_ID=$ENVIRONMENT_ID@" refactoring_assistant/config/config.properties
+sed -i "s@CERTS_LOCATION=.*@CERTS_LOCATION=$CERTS_LOCATION@" /opt/refactoring-assistant/refactoring-assistant/config/config.properties
+sed -i "s@HOST_PORT=.*@HOST_PORT=$HOST_PORT@" /opt/refactoring-assistant/refactoring-assistant/config/config.properties
+sed -i "s@ENVIRONMENT_ID=.*@ENVIRONMENT_ID=$ENVIRONMENT_ID@" /opt/refactoring-assistant/refactoring-assistant/config/config.properties
 
-chmod 766 refactoring_assistant/scripts/nls.sh
+chmod 766 /opt/refactoring-assistant/refactoring-assistant/scripts/nls.sh
 
-cd refactoring_assistant
+cd /opt/refactoring-assistant/refactoring-assistant
 ./start.sh --prepare-only /root/certs
