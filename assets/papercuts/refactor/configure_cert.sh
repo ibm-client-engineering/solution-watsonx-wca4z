995090bfac50dc837bc82dac7f37e6f6dce56c53
root <root@localhost.localdomain>
2024-04-11 08:26:15 -0700

    mistakenly forgot to commit these a while ago

--- assets/papercuts/refactor/configure_cert.sh
+++ assets/papercuts/refactor/configure_cert.sh
@@ -7,9 +7,9 @@ cp refactoring_assistant/config/template.properties refactoring_assistant/config
 
 chmod 766 refactoring_assistant/config/config.properties
 
-sed -i "s@CERTS_LOCATION=.*@CERTS_LOCATION=$CERTS_LOCATION@" config.properties
-sed -i "s@HOST_PORT=.*@HOST_PORT=$HOST_PORT@" config.properties
-sed -i "s@ENVIRONMENT_ID=.*@ENVIRONMENT_ID==$ENVIRONMENT_ID@" config.properties
+sed -i "s@CERTS_LOCATION=.*@CERTS_LOCATION=$CERTS_LOCATION@" refactoring_assistant/config/config.properties
+sed -i "s@HOST_PORT=.*@HOST_PORT=$HOST_PORT@" refactoring_assistant/config/config.properties
+sed -i "s@ENVIRONMENT_ID=.*@ENVIRONMENT_ID==$ENVIRONMENT_ID@" refactoring_assistant/config/config.properties
 
 chmod 766 refactoring_assistant/scripts/nls.sh
 
