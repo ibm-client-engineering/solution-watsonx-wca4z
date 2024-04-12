d137bddd8a2c3c056ed1cb3e7aea047b3a5f46b1
oscar-ricaud <oscar.ricaud@ibm.com>
2024-04-12 14:00:51 -0500

    commenting stuff out for testing...

--- assets/papercuts/addi/tls/tls_certs_configuration.ps1
+++ assets/papercuts/addi/tls/tls_certs_configuration.ps1
@@ -21,38 +21,39 @@ function Main {
 
       TestConnection -AddiIP $AddiIP -RefactorIP $RefactorIP -PrivateKeyPath $PrivateKeyPath
 
-      if (-not (Test-Path $CertificatePath -PathType Container)) {
-          Write-Host "Directory $CertificatePath does not exist... creating one now"
-          New-Item -ItemType Directory -Path $CertificatePath
-      } else {
-          Write-Host "Directory $CertificatePath already exists, skipping."
-      }
-      # You have to define your vars like this or it breaks...
-      $ServerCertificateFileName = "server_certificate.crt"
-      $ZookeeperFileName = "zookeeper.crt"
-
-      # Generate key pair, export and import cert to keystore
-      GenerateKeyPair -KeyPass $KeyPass -KeyStorePath $KeyStorePath -Fqdn $fqdn -AddiIP $AddiIP
-      Export-CertificateToPfx -Fqdn $fqdn -KeyPass $KeyPass -KeyStorePath $KeyStorePath -CertificatePath $CertificatePath -Filename $ServerCertificateFileName
-      # creates zookeeper.crt file
-      Export-CertificateToPfx -Fqdn $fqdn -KeyPass $KeyPass -KeyStorePath $KeyStorePath -CertificatePath $CertificatePath -Filename $ZookeeperFileName
-      Import-CertificateToKeystoreWithAlias -KeyStorePath $KeyStorePath -CertificatePath $ServerCertificateFileName -Alias "self-signed-root" -StorePass $KeyPass
-
-      ConfigureCerts -RefactorIP $RefactorIP -CertificatePath $CertificatePath -KeyPass $KeyPass -Fqdn $fqdn
-
-      ImportCertToJavaKeyStore -KeyStorePath $KeyStorePath -KeyPass $KeyPass -JreCaCertsPath $JreCaCertsPath
-
-      # Import the root certificate to the trusted root certification authorities store
-      $certificateFilePath = $env:certificatePathRoot
-      Add-RootCertificateToTrustedRoot -CertificatePath $certificateFilePath
-      Add-RootCertificateToTrustedRoot -CertificatePath "C:\certificates\combined.crt"
-
-
-      UpdateYamlFile -MyHash $MyHashPassword -AddiIP $AddiIP -RefactorIP $RefactorIP
-
-      # export zookeeper.crt to refactor host
-      ExportFileToRemoteHost -CertificatePath $CertificatePath -AddiIP $AddiIP -RefactorIP $RefactorIP -PrivateKeyPath $PrivateKeyPath
-      Write-Host "TLS configuration completed successfully."
+#
+#       if (-not (Test-Path $CertificatePath -PathType Container)) {
+#           Write-Host "Directory $CertificatePath does not exist... creating one now"
+#           New-Item -ItemType Directory -Path $CertificatePath
+#       } else {
+#           Write-Host "Directory $CertificatePath already exists, skipping."
+#       }
+#       # You have to define your vars like this or it breaks...
+#       $ServerCertificateFileName = "server_certificate.crt"
+#       $ZookeeperFileName = "zookeeper.crt"
+#
+#       # Generate key pair, export and import cert to keystore
+#       GenerateKeyPair -KeyPass $KeyPass -KeyStorePath $KeyStorePath -Fqdn $fqdn -AddiIP $AddiIP
+#       Export-CertificateToPfx -Fqdn $fqdn -KeyPass $KeyPass -KeyStorePath $KeyStorePath -CertificatePath $CertificatePath -Filename $ServerCertificateFileName
+#       # creates zookeeper.crt file
+#       Export-CertificateToPfx -Fqdn $fqdn -KeyPass $KeyPass -KeyStorePath $KeyStorePath -CertificatePath $CertificatePath -Filename $ZookeeperFileName
+#       Import-CertificateToKeystoreWithAlias -KeyStorePath $KeyStorePath -CertificatePath $ServerCertificateFileName -Alias "self-signed-root" -StorePass $KeyPass
+#
+#       ConfigureCerts -RefactorIP $RefactorIP -CertificatePath $CertificatePath -KeyPass $KeyPass -Fqdn $fqdn
+#
+#       ImportCertToJavaKeyStore -KeyStorePath $KeyStorePath -KeyPass $KeyPass -JreCaCertsPath $JreCaCertsPath
+#
+#       # Import the root certificate to the trusted root certification authorities store
+#       $certificateFilePath = $env:certificatePathRoot
+#       Add-RootCertificateToTrustedRoot -CertificatePath $certificateFilePath
+#       Add-RootCertificateToTrustedRoot -CertificatePath "C:\certificates\combined.crt"
+#
+#
+#       UpdateYamlFile -MyHash $MyHashPassword -AddiIP $AddiIP -RefactorIP $RefactorIP
+#
+#       # export zookeeper.crt to refactor host
+#       ExportFileToRemoteHost -CertificatePath $CertificatePath -AddiIP $AddiIP -RefactorIP $RefactorIP -PrivateKeyPath $PrivateKeyPath
+#       Write-Host "TLS configuration completed successfully."
 
 
 }
